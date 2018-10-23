1b8ad008ad827564ca50f8e842981f0bcd73d034
samueldemoura <samueldemouramoreira@gmail.com>
2018-10-23 19:28:47 -0300

    Ver grupos no menu principal

--- redesocial/principal.py
+++ redesocial/principal.py
@@ -15,7 +15,7 @@ class MenuPrincipal():
             opcoes = [['Logout', login_manager.logout],
                       ['Ver Perfil', cls.ver_perfil],
                       ['Ver Lista de Usuários', cls.ver_lista_usuarios],
-                      ['Ver Lista de Grupos', None], ]
+                      ['Ver Lista de Grupos', cls.ver_lista_grupos], ]
 
             opcao = menu_opcoes('MENU INICIAL', opcoes)
 
@@ -32,7 +32,14 @@ class MenuPrincipal():
 
     @classmethod
     def ver_lista_usuarios(cls):
-        DB.cursor.execute(f'SELECT name, city FROM tUser WHERE id_user != {State.usuario_atual["id_user"]}')
+        DB.cursor.execute(f'''
+            SELECT
+                id_user, name, city
+            FROM
+                tUser
+            WHERE
+                id_user != {State.usuario_atual["id_user"]}
+            ''')
         users = DB.cursor.fetchall()
 
         opcoes_usuarios = [['Cancelar']] + [[f'{user["name"]}, {user["city"]}'] for user in users]
@@ -44,8 +51,42 @@ class MenuPrincipal():
                 return
             else:
                 # TODO
+                Perfil.interagir_com_usuario(users[opcao - 1]['id_user'])
                 return
 
     @classmethod
     def ver_lista_grupos(cls):
-        pass
+        DB.cursor.execute(f'SELECT * FROM tGroup')
+        grupos = DB.cursor.fetchall()
+
+        opcoes = [['Cancelar']] + [[f'{grupo["name"]}'] for grupo in grupos]
+        opcao = menu_opcoes('INTERAGIR COM GRUPO', opcoes)
+
+        if opcao != 0:
+            DB.cursor.execute(f'''
+                SELECT
+                    status
+                FROM
+                    rUser_Group
+                WHERE
+                    id_user = {State.usuario_atual['id_user']}
+                AND
+                    id_group = {grupos[opcao - 1]['id_group']}
+                ''')
+            status = DB.cursor.fetchone()
+
+            opcoes_grupo = [
+                ['Cancelar'],
+                ['Visitar Grupo']
+                ]
+
+            if status:
+                if status == 0:
+                    opcoes_grupo.append(['Cancelar Solicitação'])
+                elif status == 1:
+                    opcoes_grupo.append(['Sair do Grupo'])
+            else:
+                opcoes_grupo.append(['Solicitar Entrada'])
+
+            opcao_grupo = menu_opcoes('INTERAGIR COM GRUPO', opcoes_grupo)
+            # TODO: tratar seleção
