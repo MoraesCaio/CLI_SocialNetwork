395c4084072e6cb163ed61c602da63648a57fe7d
samueldemoura <samueldemouramoreira@gmail.com>
2018-10-24 10:33:23 -0300

    Ver postagens, comentários e respostas no grupo

--- redesocial/grupo.py
+++ redesocial/grupo.py
@@ -56,7 +56,7 @@ class Grupo():
         opcao_grupo = menu_opcoes('INTERAGIR COM GRUPO', opcoes_grupo)
 
         if opcao_grupo == 1:
-            pass # TODO: Visitar grupo
+            cls.ver_mural()
         elif opcao_grupo == 2:
             cls.ver_membros()
         elif opcao_grupo == 3:
@@ -258,7 +258,132 @@ class Grupo():
 
     @classmethod
     def ver_mural(cls):
-        pass
+        DB.cursor.execute(f'''
+            SELECT
+                *
+            FROM
+                tPost
+            INNER JOIN
+                tUser
+            ON
+                tPost.id_user = tUser.id_user
+            WHERE
+                tPost.id_wall = {cls.grupo['id_wall']}
+            ''')
+        posts = DB.cursor.fetchall()
+
+        opcoes_postagem = [
+            ['Voltar ao menu principal'],
+            ['Criar postagem']
+        ] + [[f'-> {post["name"]}: {post["text"]}'] for post in posts]
+        opcao_postagem = menu_opcoes('INTERAGIR COM POSTAGEM', opcoes_postagem)
+
+        if opcao_postagem == 1:
+            pass  # TODO: criar postagem
+        elif opcao_postagem > 1:
+            # Interagir com uma postagem
+            post_interagido = posts[opcao_postagem - 2]
+
+            opcoes = [
+                ['Voltar ao menu principal'],
+                ['Ver comentários'],
+            ]
+            if post_interagido['id_user'] == State.usuario_atual['id_user'] or cls.logado_como_adm():
+                opcoes.append(['Remover postagem'])
+
+            opcao = menu_opcoes('INTERAGIR COM POSTAGEM', opcoes)
+
+            if opcao == 1:
+                # Ver comentários
+                DB.cursor.execute(f'''
+                    SELECT
+                        *
+                    FROM
+                        tComment
+                    INNER JOIN
+                        tUser
+                    ON
+                        tComment.id_user = tUser.id_user
+                    WHERE
+                        tComment.id_post = {post_interagido['id_post']}
+                    ''')
+                comentarios = DB.cursor.fetchall()
+
+                opcoes = [['Cancelar'], ['Comentar']] + [[f'-> {comentario["name"]}: {comentario["text"]}'] for comentario in comentarios]
+                opcao = menu_opcoes('INTERAGIR COM COMENTARIO', opcoes)
+
+                if opcao == 1:
+                    pass  # TODO: postar comentario
+                elif opcao > 1:
+                    # Interagir com comentário
+                    opcoes = [['Cancelar'], ['Responder'], ['Ver respostas']]
+
+                    # Só quem fez o comentário ou um administrador pode remove-lô
+                    if comentarios[opcao - 2]['id_user'] == State.usuario_atual['id_user'] or cls.logado_como_adm():
+                        opcoes.append(['Remover comentário'])
+
+                    opcao_comentario = menu_opcoes('INTERAGIR COM COMENTARIO', opcoes)
+                    if opcao_comentario == 1:
+                        pass  # TODO: postar resposta
+                    elif opcao_comentario == 2:
+                        # Ver respostas
+                        DB.cursor.execute(f'''
+                            SELECT
+                                *
+                            FROM
+                                tReply
+                            INNER JOIN
+                                tUser
+                            ON
+                                tReply.id_user = tUser.id_user
+                            WHERE
+                                tReply.id_comment = {comentarios[opcao - 2]['id_comment']}
+                            ''')
+                        respostas = DB.cursor.fetchall()
+
+                        opcoes_resposta = [['Cancelar'], ['Responder']] + [[f'-> {resposta["name"]}: {resposta["text"]}'] for resposta in respostas]
+                        opcao_resposta = menu_opcoes('INTERAGIR COM RESPOSTA', opcoes_resposta)
+
+                        if opcao_resposta == 1:
+                            pass  # TODO: responder
+                        elif opcao_resposta > 1:
+                            # Interagir com resposta
+                            opcoes = [['Cancelar']]
+
+                            # Só quem postou a resposta ou um administrador pode remover
+                            if respostas[opcao_resposta - 2]['id_user'] == State.usuario_atual['id_user'] or cls.logado_como_adm():
+                                opcoes.append(['Remover resposta'])
+
+                            if menu_opcoes('INTERAGIR COM RESPOSTA', opcoes) == 1:
+                                DB.cursor.execute(f'''
+                                    DELETE FROM
+                                        tReply
+                                    WHERE
+                                        id_reply = {respostas[opcao_resposta - 2]['id_reply']}
+                                    ''')
+                                print('Resposta removida.')
+                                DB.connection.commit()
+                    elif opcao_comentario == 3:
+                        # Remover comentário
+                        DB.cursor.execute(f'''
+                            DELETE FROM
+                                tComment
+                            WHERE
+                                id_comment = {comentarios[opcao - 2]['id_comment']}
+                            ''')
+                        print('Comentário removido.')
+                        DB.connection.commit()
+
+            elif opcao == 2:
+                # Remover postagem
+                DB.cursor.execute(f'''
+                    DELETE FROM
+                        tPost
+                    WHERE
+                        id_post = {post_interagido['id_post']}
+                    ''')
+                print('Postagem removida.')
+                DB.connection.commit()
 
     @classmethod
     def logado_como_adm(cls):
